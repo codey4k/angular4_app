0c6897082645c114223628e1747d54a26f74fb6b
codey4k <codey4k@gmail.com>
2017-08-17 18:34:03 +0000

    Adding header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
 import { MenuBar } from './menu_bar/menu_bar.component'
+import { Header } from './header/header.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBar
+    MenuBar,
+    Header
   ],
   imports: [
     BrowserModule
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector : 'header-tag',
+    templateUrl : './header.component.html',
+    styleUrls : ['./header.component.css']
+})
+
+export class Header {
+    public welcome = 'Welcome to my site'
+}
+
+
