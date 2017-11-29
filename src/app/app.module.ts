adffaab1ae202dd30a130f16eb442651234456f6
codey4k <codey4k@gmail.com>
2017-11-29 23:33:21 +0000

    adding services section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MenuBar } from './menu_bar/menu_bar.component'
 import { Header } from './header/header.component'
+import { Services } from './services/services.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBar,
-    Header
+    Header,
+    Services
   ],
   imports: [
     BrowserModule
---
+++ src/app/services/services.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'services',
+    template : `
+        <section class="services">
+            <div class="services__container">
+                <div class="services__left">
+                    <div class="services__left-1"></div>
+                    <div class="services__left-2"></div>
+                </div>
+                <div class="services__right">
+                    <div class="services__right-1"></div>
+                    <div class="services__right-2"></div>
+                </div>
+            </div>
+        </section>
+    `,
+    styleUrls : ['./services.component.css']
+})
+
+export class Services {
+    
+}
