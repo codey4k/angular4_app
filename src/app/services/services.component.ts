9e8e8c146c27fd4b0d5b5144600dfd0edfcc5b07
codey4k <codey4k@gmail.com>
2018-01-05 23:28:14 +0000

    update services component

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,12 +6,12 @@ import { Component } from '@angular/core'
         <section class="services">
             <div class="services__container">
                 <div class="services__left">
-                    <div class="services__left-1"></div>
-                    <div class="services__left-2"></div>
+                    <div class="services__left-1">Ceo Marketing</div>
+                    <div class="services__left-2">Web Development</div>
                 </div>
                 <div class="services__right">
-                    <div class="services__right-1"></div>
-                    <div class="services__right-2"></div>
+                    <div class="services__right-1">Brandign design</div>
+                    <div class="services__right-2">Web Security</div>
                 </div>
             </div>
         </section>
