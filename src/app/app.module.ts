136080c9142e4d62f0cc8ec559989f828952518c
codey4k <codey4k@gmail.com>
2018-03-29 22:47:53 +0000

    adding new component Description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MenuBar } from './menu_bar/menu_bar.component'
 import { Header } from './header/header.component'
 import { Services } from './services/services.component'
 import { Footer } from './footer/footer.component'
+import { Description } from './description/description.component'
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { Footer } from './footer/footer.component'
     MenuBar,
     Header,
     Services,
+    Description,
     Footer,
   ],
   imports: [
---
+++ src/app/description/description.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector : 'description-component',
+    template : `
+        <section class="description">
+        </section>
+    `,
+    styleUrls : ['./description.component.css']
+})
+
+export class Description {
+    constructor(){}
+}
