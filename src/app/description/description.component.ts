263946eb9ded3e8f5d9e260a029b86bbb0ecc52c
codey4k <codey4k@gmail.com>
2018-03-29 23:27:44 +0000

    defining styles to Description component

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -4,6 +4,11 @@ import { Component } from '@angular/core'
     selector : 'description-component',
     template : `
         <section class="description">
+            <div class="description__container">
+                <div class="description__img">
+                </div>
+                <p class="description__text">Hello world</p>
+            </div>
         </section>
     `,
     styleUrls : ['./description.component.css']
