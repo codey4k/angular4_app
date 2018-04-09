3d94d33962fc2144176c0cb3efde01a131cf45e7
codey4k <codey4k@gmail.com>
2018-04-09 17:32:18 +0000

    adding description to services items

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -9,20 +9,24 @@ import { Component } from '@angular/core'
                     <div class="services__left-1">
                         <img src="assets/services/ceo.jpeg" class="services__img" alt="ceo marqueting"/>
                         <h3 class="services__title">Ceo Marketing</h3>
+                        <p class="services__description-item">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                     </div>
                     <div class="services__left-2">
                         <img src="assets/services/webdesign.jpeg" class="services__img" alt="webdesign"/>
                         <h3 class="services__title">Web Development</h3>
+                        <p class="services__description-item">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                     </div>
                 </div>
                 <div class="services__right">
                     <div class="services__right-1">
                         <img src="assets/services/branding.jpeg" class="services__img" alt="branding"/>
                         <h3 class="services__title">Brandign design</h3>
+                        <p class="services__description-item">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                     </div>
                     <div class="services__right-2">
                         <img src="assets/services/security.jpeg" class="services__img" alt="security"/>
                         <h3 class="services__title">Web Security</h3>
+                        <p class="services__description-item">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
                     </div>
                 </div>
             </div>
